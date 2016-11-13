72f37e0625a7764e34f83c4e7c110180bb5b0433
Abraham Araujo Moreno <alex_91y@hotmail.com>
2016-11-13 01:23:21 -0600

    Add files via upload

---
+++ PruebaLista.java
@@ -0,0 +1,45 @@
+
+package Lista;
+
+public class PruebaLista {
+    
+    public static void main(String []args)
+    {
+        ListaEnlazada lista = new ListaEnlazada("ListaEnlazada");
+        
+        lista.insertarAlFrente(-1);
+        lista.imprimir();
+        lista.insertarAlFrente(0);
+        lista.imprimir();
+        lista.insertarAlFinal(1);
+        lista.imprimir();
+        lista.insertarAlFinal(5);
+        lista.imprimir();
+        
+        try
+        {
+            Object objetoElminado = lista.eliminarDelFrente();
+            System.out.printf("%s eliminado\n", objetoElminado);
+            lista.imprimir();
+            
+            objetoElminado = lista.eliminarDelFrente();
+            System.out.printf("%s eliminado\n", objetoElminado);
+            lista.imprimir();
+            
+            objetoElminado = lista.eliminarDelFinal();
+            System.out.printf("%s eliminado\n", objetoElminado);
+            lista.imprimir();
+            
+            objetoElminado = lista.eliminarDelFinal();
+            System.out.printf("%s eliminado\n", objetoElminado);
+            lista.imprimir();
+        }
+        catch(ExcepcionListaVacia exLista)
+        {
+            exLista.printStackTrace();
+        }
+    }
+
+//Fin 
+    
+}
